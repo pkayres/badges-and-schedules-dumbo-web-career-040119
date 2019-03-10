56ad57928d0a746931238150a96b6f8146f0347e
Pete A <peter.ayresmusic@gmail.com>
2019-03-10 21:22:36 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,8 +21,8 @@ end
 def printer(attendees)
   batch_badge_creator(attendees).each do |name|
     puts name
- assign_rooms(attendees).each do |name|
-     puts name
-    end
-  end
 end
+assign_rooms(attendees).each do |name|
+     puts name
+   end
+end 
