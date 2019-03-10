41b27e3e4ee22fa97b6b2cb2000d622b54db9c8d
Pete A <peter.ayresmusic@gmail.com>
2019-03-10 21:23:36 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+  end
+
+  def batch_badge_creator(attendees)
+      array = []
+        attendees.each do |name|
+          array.push(badge_maker(name))
+        end
+    return array
+  end
+
+  def assign_rooms(attendees)
+    room_assignments = []
+      attendees.each_with_index do |name, index|
+        room_assignments.push("Hello, #{name}! You'll be assigned to room #{index+1}!")
+      end
+  return room_assignments
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |name|
+    puts name
+end
+assign_rooms(attendees).each do |name|
+     puts name
+   end
+end 
